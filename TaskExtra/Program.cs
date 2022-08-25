574c976681e2768bb9cd2561c960515eb0e6801c
Nolsenus <nolsenus@mail.ru>
2022-08-25 13:46:40 +0300

    Added the extra task solution

---
+++ TaskExtra/Program.cs
@@ -0,0 +1,17 @@
+﻿int SquareSumZero() {
+    int sum = 0;
+    int squareSum = 0;
+    try {
+        do {
+            int nextNum = Convert.ToInt32(Console.ReadLine());
+            sum += nextNum;
+            squareSum += nextNum * nextNum;
+        } while (sum != 0);
+    } catch {
+        Console.WriteLine("Вы ввели не целое число.");
+    }
+    return squareSum;
+}
+
+Console.WriteLine("Вводите целые числа, чтобы закончить введите такое число, чтобы сумма всех чисел была равна 0");
+Console.WriteLine(SquareSumZero());
